f635140b0359e3599f349c0be5eb5ce6617fd445
Sameer Patil <sameerpatil38@gmail.com>
2017-06-29 09:45:03 -0700

    Fixed color space issue in drive.py

--- drive.py
+++ drive.py
@@ -3,7 +3,7 @@ import base64
 from datetime import datetime
 import os
 import shutil
-
+import cv2
 import numpy as np
 import socketio
 import eventlet
@@ -60,7 +60,8 @@ def telemetry(sid, data):
         # The current image from the center camera of the car
         imgString = data["image"]
         image = Image.open(BytesIO(base64.b64decode(imgString)))
-        image_array = np.asarray(image)
+        image_array = cv2.cvtColor(np.asarray(image), cv2.COLOR_RGB2YUV)
+        #image_array = np.asarray(image)
         steering_angle = float(model.predict(image_array[None, :, :, :], batch_size=1))
 
         throttle = controller.update(float(speed))
